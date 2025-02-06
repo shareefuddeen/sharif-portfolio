aa28512b16dfe3e3c3e036d739a26fae4562a34a
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2025-02-05 12:56:35 -1200

    2nd check

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,9 +1,11 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html", // Ensure Tailwind processes index.html
+    "./src/**/*.{js,ts,jsx,tsx}", // Target all source files
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
