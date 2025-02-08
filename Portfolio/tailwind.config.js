3197f9f89497717d3c8d30b1be2edf28ae7a927b
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2025-02-07 15:04:39 -1200

    check

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html", // Ensure Tailwind processes index.html
-    "./src/**/*.{js,ts,jsx,tsx}", // Target all source files
-  ],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sharif-portfolio/",
-  plugins: [react()]})
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+});
