ee7a5dd9eeb97840ee22efe4234115185e8e2da7
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2025-02-14 13:22:22 -1200

    analytics

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import Home from './Home';
 import './index.css';
 
 function App() {
   return (
     <div>
+      <SpeedInsights/>
+      <Analytics/>
       <Home />
     </div>
   )
